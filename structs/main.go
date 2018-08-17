e05656ad3de67aa34fea54b4f284cc5f9b44a430
Sebastian Schalow <schalowseb@gmail.com>
2018-08-17 12:39:59 +0200

    Add a nested struct to person with contact information

--- structs/main.go
+++ structs/main.go
@@ -2,13 +2,26 @@ package main
 
 import "fmt"
 
+type contactInfo struct {
+	email   string
+	zipCode int
+}
+
 type person struct {
 	firstName string
 	lastName  string
+	contactInfo
 }
 
 func main() {
-	alex := person{firstName: "Alex", lastName: "Anderson"}
-	fmt.Printf("%+v", alex)
-}
+	jim := person{
+		firstName: "Jim",
+		lastName:  "Anderson",
+		contactInfo: contactInfo{
+			email:   "jim.anderson@gmail.com",
+			zipCode: 12345,
+		},
+	}
 
+	fmt.Printf("%+v", jim)
+}
